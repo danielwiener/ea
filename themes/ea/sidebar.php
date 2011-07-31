f06dda2a510c9c9d12725a0905affe3faaef7442
danielwiener <daniel@danielwiener.com>
2011-07-31 09:29:42 -0400

    changing php header on all files

--- themes/ea/sidebar.php
+++ themes/ea/sidebar.php
@@ -3,8 +3,8 @@
  * The Sidebar containing the main widget area.
  *
  * @package WordPress
- * @subpackage Twenty_Eleven
- * @since Twenty Eleven 1.0
+ * @subpackage EA
+ * @since EA 1.0
  */
 ?>
 		<div id="secondary" class="widget-area" role="complementary">
